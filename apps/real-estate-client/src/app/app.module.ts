c4fd5b497005caccba20b4982fb15bd2c524a27e
Ndakotsu1015 <maliyu1015@gmail.com>
2023-05-26 11:35:57 +0100

    initial commit

--- apps/real-estate-client/src/app/app.module.ts
+++ apps/real-estate-client/src/app/app.module.ts
@@ -4,19 +4,14 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { AccordionModule } from 'primeng/accordion';     //accordion and accordion tab
-// import { MenuItem } from 'primeng/api';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
-    AccordionModule,
-    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
